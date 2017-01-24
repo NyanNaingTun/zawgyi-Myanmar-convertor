a932ba417a738f79a805d12530fd9fda306e7790
flamelion <mr.nyanaingtun@gmail.com>
2017-01-24 09:01:44 -0600

    make it better

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app=express()
 app.set('port',(process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-
+let token="EAAMZCuJeEMZBgBAO7tdtSO7ZCGTE8eZCTQTCEzycc3D2qzwDtSqhcqRLVgYQZC4ByZAi4oyJZApwZAfPpuxN7QzEVNmuZBoeKR7HVpTIIFTKWroDjnMfKfJWVaW1kmmLfY26LmDD9AVdMm9YGK8da5btQ1o4oy5ZAZBwXhZBVmjYVc8wXAZDZD"
 app.get('/',function(req,res){
 	res.send("Hi I am a chatbot")
 })
@@ -20,6 +20,43 @@ app.get('/webhook/',function(req,res)
 	}
 	res.send("Wrong Token")
 })
+app.post('/webhook/',function(req,res)
+{
+	let messaging_events=req.body.entry[0].messaing_events
+	for(let i=0;i<messaging_events.length;i++)
+	{
+		let event =messaging_events[i]
+		let sender=event.sender.id
+		if(event.message && event.message.text)
+		{
+		  let text=sender+"! \n'I am bot. I am saying as you say:"+event.message.text
+		  sendText(sender,text)
+		}
+	}
+	res.sendStatus(200)
+})
+function sendText(sender,text)
+{
+	let messageData={text:text}
+	request(
+	{
+		url:"https://graph.facebook.com/v2.6/me/messages",
+		qs: {access_token,token},
+	        method: "POST",
+		json:{
+			recept:{id:sender},
+			message:messageData
+		},function(error,response,body){
+			if(error)
+			{	console.log("sending errror")}
+			else if(response.body.error)
+			{
+				console.log("response body error")
+			}
+		}
+
+	})
+}
 app.listen(app.get('port'),function(){
 	console.log("running: port")
 })
