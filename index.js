0e7ccf5948fcdf65a594dbf56e3782218e617076
flamelion <mr.nyanaingtun@gmail.com>
2017-01-24 09:31:21 -0600

    make it better

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/webhook/',function(req,res)
 })
 app.post('/webhook/',function(req,res)
 {
-	let messaging_events=req.body.entry[0].messaging_events
+	let messaging_events=req.body.entry[0].messaging
 	for(let i=0;i<messaging_events.length;i++)
 	{
 		let event =messaging_events[i]
