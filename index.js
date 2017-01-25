a9725eaf02d31c628c7c13481bc39d8076b1db02
flamelion <mr.nyanaingtun@gmail.com>
2017-01-25 10:50:42 -0600

    commit for deploy to heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express=require('express')
 const bodyParser=require('body-parser')
 const request=require('request')
 const app=express()
-const hello={}
+let hello={}
 app.set('port',(process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
