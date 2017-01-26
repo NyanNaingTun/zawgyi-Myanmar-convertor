d4d8053c58b05481f3cef2683ba51ba489bd4240
flamelion <mr.nyanaingtun@gmail.com>
2017-01-26 11:45:59 -0600

    my first commit

--- index.js
+++ index.js
@@ -27,7 +27,12 @@ app.post('/webhook/',function(req,res)
 	for(let i=0;i<messaging_events.length;i++)
 	{
 		let event =messaging_events[i]
+		console.log(event)
+		console.log("----------")
+		console.log(event.sender)
 		let sender=event.sender.id
+		conse.log(event.message)
+		console.log("----------")
 	        if(event.message && event.message.text)
 		{
 			let message=event.message.text
@@ -48,12 +53,12 @@ app.post('/webhook/',function(req,res)
 			else if(message.indexOf('HELP'.toUpperCase())>-1)
 			{
 				console.log(sender+"-"+"help")
-				reply="First, You must be resgister in my system and then Pass_code for security reason. The system gave you API key and save your Passcode(Refer..1 and 2)After that, you  can add command further,you can request using it from your IOT(Ref:3). In your IOT. you can use get request the following url:\n https://flamelion.herokuapp.com/action?api={api_key}&pc={pass_code}&com={key_command}\n In messager, To send command with value, you message to us like that {key_command} {value}. \n\n For Display list of command. Type \" avaiable_command\" " 
+				reply="First, You must be resgister in my system and then Pass_code for security reason. The system gave you API key and save your Passcode(Refer..1 and 2)After that, you  can add command further,you can request using it from your IOT(Ref:3). In your IOT. you can use get request the following url:\n https://flamelion.herokuapp.com/action?api={api_key}&pc={pass_code}&com={key_command}\n In messager, To send command with value, you message to us like that {key_command} {value}. \n\n For Display list of command. Type \" Avaliable_Command\" " 
 			}
 			else if(message.indexOf('ABOUT'.toUpperCase())>-1)
 			{
 				console.log(sender+"-"+"About")
-				reply="We purpose for IOT leaner. Not for Commerical use. Owner.. flamelion Nyan"
+				reply="We purpose for IOT learner. Not for Commerical use. Owner.. flamelion Nyan"
 			}
 			else
 			{
