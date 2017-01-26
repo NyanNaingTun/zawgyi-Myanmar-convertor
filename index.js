468c817091830461c3762813f23965f00fa5aca6
flamelion <mr.nyanaingtun@gmail.com>
2017-01-26 12:14:44 -0600

    my first commit

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ app.post('/webhook/',function(req,res)
 			let message=event.message.text
 			let reply=""
 			message=message.toUpperCase()
-			if(message.indexOf('AVALIABLE_COMMAND') >-1)
+			if(message.indexOf('COMMAND_LIST') >-1)
 			{
 				console.log(sender +"-"+"type help")		
-				reply="Avaliable commandlines.\n1. Register\n2. Pass_code {key_code}\n3. Add_Command {key_command}\n4. Remove_command {key_command}\n5. List_command\n6. help\n7. About\t The word {word} will be your desired word that should not included special characters{-\"_,#$!...etc} and space."
+				reply="Avaliable commandlines.\n1. Register\n2. Pass_code {key_code}\n3. Add_Command {key_command}\n4. Remove_command {key_command}\n5. Command_List\n6. help\n7. About\t The word {word} will be your desired word that should not included special characters{-\"_,#$!...etc} and space."
 			}
 			else if(message.indexOf('   ')>-1)			{
 //			 var fs = require('fs');
@@ -54,7 +54,7 @@ app.post('/webhook/',function(req,res)
 			else if(message.indexOf('HELP'.toUpperCase())>-1)
 			{
 				console.log(sender+"-"+"help")
-				reply="First, You must be register in our system and then Pass_code for security reason.\n The system gives you API key and save your Passcode(Refer..1 and 2)\nAfter that, you  can add command further,you can request using it from your IOT(Ref:3).\n   In your IOT. you can use get request the following url:\n https://flamelion.herokuapp.com/action?api={api_key}&pc={pass_code}&com={key_command}\n In messager, To send command with value, you message to us like that {key_command} {value}. \n\n For Display List of Commands. Type \" Avaliable_Command\" " 
+				reply="First, You must be register in our system and then Pass_code for security reason.\n The system gives you API key and save your Passcode(Refer..1 and 2)\nAfter that, you  can add command further,you can request using it from your IOT(Ref:3).\n   In your IOT. you can use get request the following url:\n https://flamelion.herokuapp.com/action?api={api_key}&pc={pass_code}&com={key_command}\n In messager, To send command with value, you message to us like that {key_command} {value}. \n\n For Display List of Commands. Type \" Command_List \" " 
 			}
 			else if(message.indexOf('ABOUT'.toUpperCase())>-1)
 			{
